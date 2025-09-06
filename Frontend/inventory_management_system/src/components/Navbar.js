b0b7603b63273668061de19e6f5fab2bdbe625b2
Ankit Patel <ankitpatel6684@gmail.com>
2025-09-06 21:23:47 +0530

    Initial commit for IMS project

--- Frontend/inventory_management_system/src/components/Navbar.js
+++ Frontend/inventory_management_system/src/components/Navbar.js
@@ -5,7 +5,8 @@ export default function Navbar(props) {
     <div>
       <navBar className="navbar navbar-expand-lg bg-danger">
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedCo
+          ntent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
